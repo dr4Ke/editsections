c06c502628af46bc70b1a5c16f485e0c3c05784c
Christophe Drevet <dr4ke@dr4ke.net>
2010-05-11 13:21:18 +0200

    french translation

---
+++ lang/fr/settings.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * french language file for editsections plugin
+ *
+ * @author Christophe Drevet <dr4ke@dr4ke.net>
+ */
+
+$lang['plugin_settings_name'] = 'Plugin de réorganisation des éditions de sections';
+
+$lang['order_type'] = 'Organisation';
+
+$lang['order_type_o_0'] = 'Une section à la fois';
+$lang['order_type_o_1'] = 'Hiérarchique (édition des sous-sections)';
+
+//Setup VIM: ex: et ts=4 enc=utf-8 :
