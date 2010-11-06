cb449333b6abe3dc06c2a5b618ea0b403931c47c
Christophe Drevet <dr4ke@dr4ke.net>
2010-11-06 15:59:12 +0100

    fix include infoutils.php for 20091225c release

--- action/es.php
+++ action/es.php
@@ -14,6 +14,7 @@ if (!defined('DOKU_TAB')) define('DOKU_TAB', "\t");
 if (!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN',DOKU_INC.'lib/plugins/');
 
 require_once DOKU_PLUGIN.'action.php';
+include_once DOKU_INC.'inc/infoutils.php';
 
 class action_plugin_editsections_es extends DokuWiki_Action_Plugin {
 
