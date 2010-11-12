f5d59498ac68ae2eec1dd936e549e6f7dbaac34e
Christophe Drevet <dr4ke@dr4ke.net>
2010-11-12 14:16:25 +0100

    disable cache completely
    othewise, the buttons are there but don't have the good values :
    . edit only the current section and no subsection
    . name of the previous section appears

--- action/es.php
+++ action/es.php
@@ -31,6 +31,7 @@ class action_plugin_editsections_es extends DokuWiki_Action_Plugin {
 			// 2009 or earlier version
 			$controller->register_hook('PARSER_HANDLER_DONE', 'BEFORE', $this, 'rewrite_sections_legacy');
 		}
+		$controller->register_hook('PARSER_CACHE_USE', 'BEFORE', $this, '_cache_use');
 		$controller->register_hook('DOKUWIKI_STARTED', 'AFTER', $this, '_addconf');
 	}
 
@@ -74,6 +75,10 @@ class action_plugin_editsections_es extends DokuWiki_Action_Plugin {
         }
     }
 
+	function _cache_use(&$event, $ags) {
+		dbglog('PARSER_CACHE_USE hook', 'editsections plugin');
+		$event->_default = 0;
+	}
 	function rewrite_sections(&$event, $ags) {
 		// get the instructions list from the handler
 		$calls =& $event->data->calls;
